796f6627cf967ae915a288702dd4be6f187e58e1
Arshadkuki2004 <arshadkumar011@gmail.com>
2025-02-04 17:44:43 -0500

    added 4 test cases
    Added 4 tests.

--- Assignment_2/Assignment_2.nUnitTests/obj/Debug/net8.0/Assignment_2.nUnitTests.AssemblyInfo.cs
+++ Assignment_2/Assignment_2.nUnitTests/obj/Debug/net8.0/Assignment_2.nUnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment_2.nUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c9f5ea515053c68c8803865e6a3977f5cedcf52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45ee0c44473de3d03cb59115b60528529cbd2076")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment_2.nUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment_2.nUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
